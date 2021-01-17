ee8511d2d7a9e52219045bd11e4053ac6f735743
MrDenySU <mrdenyru@yandex.com>
2021-01-17 23:57:40 +1000

    upd

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 /* Import */
 import express from "express";
 import mongoose from "mongoose";
+
 import session from "express-session";
 import mongodb_session_store from "connect-mongodb-session";
+
 import compression from "compression";
+import minify from "express-minify";
+import helmet from "helmet";
+import favicon from "serve-favicon";
 
 import { join, resolve } from "path";
 
 import { PORT, db, secret } from "./config.js";
 import { error, success } from "./helpers/logger.js";
 
+/* Option */
+const __dirname = resolve();
+
 /* App */
 const app = express();
 
+// View (render)
+
+
 // Security
-app.disable("x-powered-by");
+app.use(helmet());
 
 // Session
 const MongoDBStore = mongodb_session_store(session);
@@ -36,17 +47,22 @@ app.use(
 );
 
 // Static files
-app.use(compression())
-app.use("/public", express.static(join(resolve(), "views/public")))
+app.use(compression());
+app.use(
+  minify({
+    cache: false,
+  })
+);
+app.use(favicon(join(__dirname, "views/public/favicon.ico")));
+app.use("/public", express.static(join(__dirname, "views/public")));
 
 // Row as JSON string to res.body
-app.use(express.json())
+app.use(express.json());
 
 // Router
 app.get("/", (req, res) => {
-  res.json({
-    session: req.session,
-    session_id: req.sessionID
+  res.render("main.hbs", {
+    title: "Main text",
   });
 });
 
